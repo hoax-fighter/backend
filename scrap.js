b6c2db19d848f301e17d5088d2ae8af65591e851
Bhirawa Mbani <bhirmbani@gmail.com>
2017-06-07 14:46:17 +0700

    adding check userInput with turnbackhoax title

--- scrap.js
+++ scrap.js
@@ -1,23 +1,44 @@
 let axios = require('axios');
 let cheerio = require('cheerio');
+let test = require('./similarityCheck');
 
 // let base_url = 'http://www.bca.co.id/id/Individu/Sarana/Kurs-dan-Suku-Bunga/Kurs-dan-Kalkulator';
 // let base_url = 'https://www.turnbackhoax.id/2017/03/';
 let base_url = 'https://www.turnbackhoax.id/2017/02/';
+// https://www.turnbackhoax.id/2017/03/
+// https://www.turnbackhoax.id/2017/01/
+// https://www.turnbackhoax.id/2017/01/page/2/
+// https://www.turnbackhoax.id/2016/12/
+// https://www.turnbackhoax.id/2016/12/page/2/
+// let arrOfUrl = ['https://www.turnbackhoax.id/2017/02/']
 
 axios.get(base_url).then((response) => {
     let $ = cheerio.load(response.data);
-    let kurs = [];
+    let source = [];
     $('a', '.entry-title').each((i, elm) => {
-        kurs.push({
+        source.push({
             title: $(elm).text().trim(),
             href: $(elm).attr('href')
         });
     });
-    return (kurs);
+    return (source);
 })
-    .then((kurs) => {
-        console.log(kurs[0].title);
+    .then((source) => {
+        let userInput = 'Banjir Di Depan Istana';
+        source.map((val, idx) => {
+            if(val.te)
+            console.log('jarowinkler', test.methods.jarowinkler(val.title.slice(0,), userInput));
+            console.log('levenshtein', test.methods.levenshtein(val.title, userInput));
+            console.log('stringSimilarity', test.methods.stringSimilarity(val.title, userInput));
+        })
+        console.log(source)
+        // console.log(test.methods.jarowinkler(source[0].title, source[1].title));
+        // console.log(test.methods.levenshtein(source[0].title, source[1].title));
+        // console.log(test.methods.stringSimilarity(source[0].title, source[1].title));
     });
+
+    // console.log(test.methods.jarowinkler('as', 'as'))
+    // console.log(test.methods.levenshtein('as', 'as'))
+    // console.log(test.methods.stringSimilarity('as', 'as'))
 
 
--- similarityCheck.js
+++ similarityCheck.js
@@ -4,18 +4,47 @@ const stringSimilarity = require('string-similarity');
 let string1 = 'bumi datar';
 let string2 = 'bumi itu datar';
 
-console.log('string 1: ', string1);
-console.log('string 2: ', string2);
+// console.log('string 1: ', string1);
+// console.log('string 2: ', string2);
+
+const methods = {};
+
+methods.jarowinkler = (str1, str2) => {
+  let one = str1.toLowerCase();
+  let two = str2.toLowerCase();
+  let temp = wuzzy.jarowinkler(one, two);
+  return (Math.round(temp * 100) / 100) * 100;
+}
+
+methods.levenshtein = (str1, str2) => {
+  let one = str1.toLowerCase();
+  let two = str2.toLowerCase();
+  let temp = wuzzy.levenshtein(one, two);
+  return (Math.round(temp * 100) / 100) * 100;
+}
+
+methods.stringSimilarity = (str1, str2) => {
+  let one = str1.toLowerCase();
+  let two = str2.toLowerCase();
+  let temp = stringSimilarity.compareTwoStrings(one, two);
+  return (Math.round(temp * 100) / 100) * 100;
+}
 
 // Jaro-Winkler method kurang OK sepertinya..
-let jarowinkler = wuzzy.jarowinkler(string1.toLowerCase(), string2.toLowerCase());
-let jarowinklerResult = (Math.round(jarowinkler * 100) / 100) * 100;
-console.log(`jaro-winkler method: ${jarowinklerResult} %`);
+// let jarowinkler = wuzzy.jarowinkler(string1.toLowerCase(), string2.toLowerCase());
+// let jarowinklerResult = (Math.round(jarowinkler * 100) / 100) * 100;
+// console.log(`jaro-winkler method: ${jarowinklerResult} %`);
 
-let levenshtein = wuzzy.levenshtein(string1.toLowerCase(), string2.toLowerCase());
-let levenshteinResult = (Math.round(levenshtein * 100) / 100) * 100;
-console.log(`levenshtein method: ${levenshteinResult} %`);
+// let levenshtein = wuzzy.levenshtein(string1.toLowerCase(), string2.toLowerCase());
+// let levenshteinResult = (Math.round(levenshtein * 100) / 100) * 100;
+// console.log(`levenshtein method: ${levenshteinResult} %`);
 
-let dice = stringSimilarity.compareTwoStrings(string1.toLowerCase(), string2.toLowerCase());
-let diceResult = (Math.round(dice * 100) / 100) * 100;
-console.log(`sorensen-dice method: ${diceResult} %`);
+// let dice = stringSimilarity.compareTwoStrings(string1.toLowerCase(), string2.toLowerCase());
+// let diceResult = (Math.round(dice * 100) / 100) * 100;
+// console.log(`sorensen-dice method: ${diceResult} %`);
+
+
+
+module.exports = {
+  methods
+}
