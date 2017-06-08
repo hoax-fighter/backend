e140817f4f84f27fc9873eb23081a2abcec52650
diditaditya <di2t.aditya@gmail.com>
2017-06-08 14:47:25 +0700

    moves hoax rules to helper

---
+++ helper/hoaxRulesCheck.js
@@ -0,0 +1,69 @@
+const wuzzy = require('wuzzy');
+
+const limits = {
+  expressive: 0.7,
+  capital: 0.5
+};
+
+const rules = {};
+
+const overlyExpressive = ['heboh',
+                          'waspada',
+                          'urgent',
+                          'mantap',
+                          'mengejutkan',
+                          'menggemparkan',
+                          'parah!',
+                          'wow!',
+                          'WHOOPS'
+                          ];
+
+rules.expressive = (string) => {
+
+  const wordsInString = string.split(' ');
+
+  let count = 0;
+
+  overlyExpressive.map((exp) => {
+    wordsInString.map((word) => {
+      if (wuzzy.levenshtein(exp, word.toLowerCase()) > limits.expressive) {
+        count += 1;
+      }
+    });
+  });
+
+  if (count > 0) {
+    return true;
+  }
+
+  return false;
+}
+
+rules.exclamation = (string) => {
+  if (/!!+/g.test(string)) {
+    return true;
+  }
+
+  return false;
+}
+
+rules.capital = (string) => {
+  let capital = 0;
+  let combinedLetters = string.replace(/\s/g, '');
+  let letters = combinedLetters.split('');
+  letters.map((letter) => {
+    if (letter === letter.toUpperCase() && /[A-Za-z]/.test(letter)) {
+      capital += 1;
+    }
+  });
+
+  if (capital/(letters.length) > limits.capital) {
+    return true;
+  }
+
+  return false;
+
+}
+
+
+module.exports = rules;
