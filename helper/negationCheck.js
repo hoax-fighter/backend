ec3692a76eb7199767b3f170c36f01e1c2842ac3
diditaditya <di2t.aditya@gmail.com>
2017-06-08 14:03:48 +0700

    adds negation check in helper and its testing

--- helper/negationCheck.js
+++ helper/negationCheck.js
@@ -2,19 +2,137 @@ const similarity = require('./similarityCheck');
 
 const negations = ['tidak', 'tak', 'bukan', 'hampir', 'nyaris', 'gagal'];
 
+const stringNegationCheck = (string, negations) => {
+  let result = {};
+  let foundNegations = [];
+  let stringWithoutNegation = '';
+  negations.map((negation) => {
+    let pattern = new RegExp(negation);
+    if (pattern.test(string)) {
+      foundNegations.push(negation);
+    }
+  });
+
+  // console.log(foundNegations);
+
+  foundNegations.map((negation) => {
+    stringWithoutNegation = string.replace(`${negation}`, '');
+  });
+
+  result.foundNegations = foundNegations;
+  result.stringWithoutNegation = stringWithoutNegation;
+
+  return result;
+
+}
+
+
+const arrayNegationCheck = (array, negations) => {
+
+  let result = {};
+  let foundNegations = [];
+  let titlesWithoutNegation = [];
+  array.map((title) => {
+    negations.map((negation) => {
+      let pattern = new RegExp(negation);
+      if (pattern.test(title)) {
+        foundNegations.push(negation);
+        titlesWithoutNegation.push(title.replace(`${negation}`, ''));
+      }
+    });
+  });
+
+  result.foundNegations = foundNegations;
+  result.titlesWithoutNegation = titlesWithoutNegation;
+
+  return result;
+
+
+}
+
+
+
 const negationCheck = (string, titleArray) => {
   let result = {};
   if (string && titleArray && titleArray.length > 0) {
-    if(typeof string === 'string' && typeof titleArray === 'array') {
+    if(typeof string === 'string' && Array.isArray(titleArray)) {
+
+      const negationInString = stringNegationCheck(string, negations).foundNegations;
+      // console.log(negationInString);
+
+      if (negationInString.length > 0) {
+
+        let probableTitles = [];
+
+        titleArray.map((title, index) => {
+          // console.log(similarity.averagedSimilarity(string, title).value);
+          if (similarity.averagedSimilarity(string, title).value > 75) {
+            probableTitles.push(title);
+          }
+        });
+
+        if (probableTitles.length > 0) {
+
+          const stringCheckResult = stringNegationCheck(string, negations);
+
+          let maxSimilarity = 0;
+
+          titleArray.map((title, index) => {
+            let value = similarity.averagedSimilarity(stringCheckResult.stringWithoutNegation, title).value;
+            // console.log(title);
+            // console.log(value);
+            if (value > maxSimilarity) {
+              maxSimilarity = value;
+              result.value = value;
+              result.title = title;
+            }
+          });
+
+          if (maxSimilarity > 90) {
+
+            result.status = 'success';
+            result.result = true;
+            result.message = 'the string without negation finds similarity > 90 %, most likely a hoax';
+            // console.log(result);
+            return result;
+
+          }
+
+          result.status = 'success';
+          result.result = false;
+          result.message = 'the string without negation does not find similarity > 90 %, probably not a hoax';
+          // console.log(result);
+          return result;
+
+        }
+
+        // console.log(probableTitles);
+
+        result.status = 'success';
+        result.result = false;
+        result.message = 'no title is higher than 75% in similarity';
+        // console.log(result);
+        return result;
+      }
+
+      result.status = 'success';
+      result.result = false;
+      result.message = 'negation is not found in the string';
+      // console.log(result);
+      return result;
 
     }
+
     result.status = 'error';
     result.message = 'first params must be string and second params must be an array of strings';
     return result;
+
   }
+
   result.status = 'error';
   result.message = 'string and string array must not be empty';
   return result;
+
 }
 
 module.exports = negationCheck;
--- test/negationCheck.js
+++ test/negationCheck.js
@@ -17,9 +17,20 @@ describe('Negation check', function() {
       negation(123, 'asd').message.should.equal('first params must be string and second params must be an array of strings');
     });
 
-    it('should return success status and ', function() {
-      negation(123, 'asd').status.should.equal('error');
-      negation(123, 'asd').message.should.equal('first params must be string and second params must be an array of strings');
+    it('should return result true if "madrid tidak juara" and "madrid juara" are compared', function() {
+      const string = 'madrid tidak juara';
+      const array = ['madrid juara', 'juventus bukan payah'];
+      negation(string, array).status.should.equal('success');
+      negation(string, array).result.should.equal(true);
+      negation(string, array).message.should.equal('the string without negation finds similarity > 90 %, most likely a hoax');
+    });
+
+    it('should return result false if "madrid tidak juara" and "persib juara" are compared', function() {
+      const string = 'madrid tidak juara';
+      const array = ['persib juara', 'juventus bukan payah'];
+      negation(string, array).status.should.equal('success');
+      negation(string, array).result.should.equal(false);
+      negation(string, array).message.should.equal('no title is higher than 75% in similarity');
     });
 
   });
