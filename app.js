e9d06dfd4767885700138cdc05a86361ba8a68ca
Bhirawa Mbani <bhirmbani@gmail.com>
2017-06-15 09:00:41 +0700

    before deploy 0.0.9

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
-app.use(function(req, res, next) {
- res.header("Access-Control-Allow-Origin", "*")
- res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
- next()
-})
+// app.use(function(req, res, next) {
+//  res.header("Access-Control-Allow-Origin", "*")
+//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+//  next()
+// })
 
 app.use(cors());
 // mongoose setup
